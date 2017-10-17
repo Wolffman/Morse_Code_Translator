34699e49e9e9a94f7c102bdfee96750103917de3
Wam <samuelwolff450@gmail.com>
2017-10-17 10:14:05 -0400

    Created get String function

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
 /**
  * Created by student on 10/17/17.
  */
-// ye
+import java.util.Scanner;
 public class Main {
+    public static String getString(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter english or morse code to translate");
+        String ans = sc.nextLine();
+        sc.close();
+    return ans;
+    }
     public static void main(String[] args) {
 
+
+
     }
+
 }
